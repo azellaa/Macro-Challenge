0e390441813e6277e42b1724ad7b8ffd5b6b7b56
Azella Gania Mutyara <azellamutyara@gmail.com>
2023-09-21 09:56:31 +0700

    Initial Commit

---
+++ MacroCoba"/MacroCoba"/MacroCoba_App.swift
@@ -0,0 +1,17 @@
+//
+//  MacroCoba_App.swift
+//  MacroCoba"
+//
+//  Created by Azella Mutyara on 22/09/23.
+//
+
+import SwiftUI
+
+@main
+struct MacroCoba_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
